b00a6b650fcd470c65975073779958322c4b58aa
LiangLi <lli@kidaptive.com>
2014-06-21 10:46:45 -0700

    Initial Commit

---
+++ Test/Test/Test1AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Test1AppDelegate.h
+//  Test
+//
+//  Created by LiangLi on 6/21/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Test1AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Test/Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Test
+//
+//  Created by LiangLi on 6/21/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Test1AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Test1AppDelegate class]));
+    }
+}
