3b134d314708be6125a5e8182af00f8de78a8896
Lei Zhao <lei.zhao@yale.edu>
2014-07-07 16:11:09 -0400

    Hue: compile went through

--- BLE_Hue/BLE Test/BLEAppDelegate.h
+++ BLE_Hue/BLE Test/BLEAppDelegate.h
@@ -5,14 +5,42 @@
 //  Copyright (c) 2013 Adafruit Industries. All rights reserved.
 //
 
+#define UIAppDelegate  ((BLEAppDelegate *)[[UIApplication sharedApplication] delegate])
+
 #import <UIKit/UIKit.h>
 
+#import "PHBridgeSelectionViewController.h"
+#import "PHBridgePushLinkViewController.h"
+#import <HueSDK_iOS/HueSDK.h>
+
 @class BLEMainViewController;
+@class PHHueSDK;
 
 @interface BLEAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
+@property (strong, nonatomic) UINavigationController *navigationController;
+
 @property (strong, nonatomic) BLEMainViewController *mainViewController;
+
+@property (strong, nonatomic) PHHueSDK *phHueSDK;
+
+#pragma mark - HueSDK
+
+/**
+ Starts the local heartbeat
+ */
+- (void)enableLocalHeartbeat;
+
+/**
+ Stops the local heartbeat
+ */
+- (void)disableLocalHeartbeat;
+
+/**
+ Starts a search for a bridge
+ */
+- (void)searchForBridgeLocal;
 
 @end
