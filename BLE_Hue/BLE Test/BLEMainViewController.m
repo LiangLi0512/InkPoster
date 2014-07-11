67f014a6fe32da1aef74e4e5fcce731c1b7cb85b
Lei Zhao <lei.zhao@yale.edu>
2014-07-10 21:20:22 -0400

    MainViewController 有问题

--- BLE_Hue/BLE Test/BLEMainViewController.m
+++ BLE_Hue/BLE Test/BLEMainViewController.m
@@ -497,7 +497,7 @@ NSTimeInterval touchRightTime = 0.0;
             //send data to UART Controller
             [_uartViewController receiveData:newData];
             
-            // Convert NSData into int, reference UARTViewController
+            //Convert NSData into int, reference UARTViewController
             int dataLength = (int)newData.length;
             NSInteger intValue;
             [newData getBytes:&intValue length:sizeof(intValue)];
@@ -508,7 +508,7 @@ NSTimeInterval touchRightTime = 0.0;
             NSLog(@"value = %i", value);
             
             // by Lei:   Hue
-            if (START_HUE <= value) {
+            if (START_HUE == value) {
                 PHBridgeResourcesCache *cache = [PHBridgeResourcesReader readBridgeResourcesCache];
                 id<PHBridgeSendAPI> bridgeSendAPI = [[[PHOverallFactory alloc] init] bridgeSendAPI];
                 
@@ -516,25 +516,10 @@ NSTimeInterval touchRightTime = 0.0;
                     
                     PHLightState *lightState = [[PHLightState alloc] init];
                     
-                    //[lightState setHue:[NSNumber numberWithInt:arc4random() % MAX_HUE]];
+                    [lightState setHue:[NSNumber numberWithInt:arc4random() % MAX_HUE]];
                     //[lightState setHue:[NSNumber numberWithInt:(value % 400)/400.0 * MAX_HUE]];
-                    [lightState setHue:[NSNumber numberWithInt:46238]];
-                    if (value>40&&value<80)
-                        [lightState setBrightness:[NSNumber numberWithInt:0]];
-                    else if (value>=80 && value <120)
-                        [lightState setBrightness:[NSNumber numberWithInt:40]];
-                    else if (value>=120 && value <160)
-                        [lightState setBrightness:[NSNumber numberWithInt:80]];
-                    else if (value>=160 && value <200)
-                        [lightState setBrightness:[NSNumber numberWithInt:120]];
-                    else if (value>=200 && value <240)
-                        [lightState setBrightness:[NSNumber numberWithInt:160]];
-                    else if (value>=240 && value <280)
-                        [lightState setBrightness:[NSNumber numberWithInt:200]];
-                    else if (value>=280 && value <320)
-                        [lightState setBrightness:[NSNumber numberWithInt:230]];
-                    else if (value>=320 && value <400)
-                        [lightState setBrightness:[NSNumber numberWithInt:254]];
+                    //[lightState setHue:[NSNumber numberWithInt:46238]];
+                    [lightState setBrightness:[NSNumber numberWithInt:254]];
                     [lightState setSaturation:[NSNumber numberWithInt:254]];
                     
                     // Send lightstate to light
